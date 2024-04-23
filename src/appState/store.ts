6b3b1b2ebc9f2f6ff956dc835e71a15464313d5b
Shota <sh.karkashadze@gmail.com>
2024-04-23 20:28:13 +0400

    Remove counter api

--- src/appState/store.ts
+++ src/appState/store.ts
@@ -1,10 +1,8 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit'
-import counterReducer from './features/counter/counterSlice'
 import usersReducer from 'appState/features/users/usersSlice'
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     users: usersReducer,
   },
 })
